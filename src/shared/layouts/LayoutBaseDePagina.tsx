198664b20ae78d51e53675ae74ac24aa9a4b7c5c
Jurandi Riqueti Junior <jurandihd4000@gmail.com>
2024-03-01 23:22:32 -0300

    feat: finalizado layout base de páginas

--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -15,16 +15,23 @@ export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ childre
 
     const smDown =  useMediaQuery(theme.breakpoints.down("sm"));
 
+    const mdDown =  useMediaQuery(theme.breakpoints.down("md"));
+
     const { toggleDrawerOpen } = useDrawerContext();
 
     return(
         <Box height="100%" display="flex" flexDirection="column" gap={1}>
-            <Box padding={1} display="flex" alignItems="center" height={theme.spacing(12)} gap={1}>
+            <Box padding={1} display="flex" alignItems="center" height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)} gap={1}>
                 {smDown && (<IconButton onClick={toggleDrawerOpen}>
                     <Icon>menu</Icon>
                 </IconButton>)}
 
-                <Typography variant="h5">
+                <Typography 
+                    variant={smDown ? "h5" : mdDown ? "h4" : "h3"}
+                    overflow="hidden"
+                    whiteSpace="nowrap"
+                    textOverflow="ellipsis"
+                >
                     {titulo}
                 </Typography>
             </Box>
